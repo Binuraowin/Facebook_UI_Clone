b697445cce48f68bfb643510205a9be54d3c347a
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-02 23:11:23 +0530

    status basic

--- newsfeed_ui_2/lib/data/data.dart
+++ newsfeed_ui_2/lib/data/data.dart
@@ -10,11 +10,13 @@ final List<User> onlineUsers = [
     name: 'David Brooks',
     imageUrl:
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  
   ),
   User(
     name: 'Jane Doe',
     imageUrl:
         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  
   ),
   User(
     name: 'Matthew Hinkle',
