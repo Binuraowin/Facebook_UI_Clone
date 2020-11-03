6147e9a0d5099acaff7442f82f00f9fddd4ceb10
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-11-03 11:50:10 +0530

    post container header

--- newsfeed_ui_2/lib/widgets/post_container.dart
+++ newsfeed_ui_2/lib/widgets/post_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:newsfeed_ui_2/data/data.dart';
 import 'package:newsfeed_ui_2/models/models.dart';
+import 'package:newsfeed_ui_2/widgets/profile_avatar.dart';
 
 class PostContainer extends StatelessWidget {
   final Post post;
@@ -9,8 +11,47 @@ class PostContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 5.0),
-      height: 100.0,
-      color: Colors.red,
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _postHeader(post: post)
+        ],
+      ),
+    );
+  }
+}
+
+class _postHeader  extends StatelessWidget {
+  final Post post;
+
+  const _postHeader({Key key,@required this.post}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(width:8.0),
+        Column(
+          children: <Widget>[
+            Text(post.user.name),
+            Row(
+              children: <Widget>[
+                Text('${post.timeAgo} .',
+                style:  TextStyle(color: Colors.grey[600],
+                fontSize: 12.0
+                ),
+                ),
+                Icon(Icons.public,
+                color: Colors.grey[600],
+                size: 12.0
+                )
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
